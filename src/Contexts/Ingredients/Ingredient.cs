8fed28a8e00f10650bc7e4ac43de6bab1e365efc
jeremie <ownedstuff@hotmail.fr>
2024-04-13 13:24:51 -0400

    feat: Add scriptable ingredients

--- src/Contexts/Ingredients/Ingredient.cs
+++ src/Contexts/Ingredients/Ingredient.cs
@@ -1,1 +1,32 @@
-﻿
+﻿public class Ingredient
+{
+    public string Id { get; set; }
+    public string DisplayName { get; set; }
+    public string Icon { get; set; }
+    public string ProcessedIcon { get; set; }
+    public SummoningValue Emotion { get; set; }
+    public SummoningValue Species { get; set; }
+    public SummoningValue Elements { get; set; }
+
+    public Ingredient(string id, string displayName, string icon, string processedIcon, SummoningValue emotion, SummoningValue species, SummoningValue elements)
+    {
+        Id = id;
+        DisplayName = displayName;
+        Icon = icon;
+        ProcessedIcon = processedIcon;
+        Emotion = emotion;
+        Species = species;
+        Elements = elements;
+    }
+
+    public Ingredient(string id, string displayName, string icon, string processedIcon, float emotion, float species, float elements)
+    {
+        Id = id;
+        DisplayName = displayName;
+        Icon = icon;
+        ProcessedIcon = processedIcon;
+        Emotion = new SummoningValue(emotion);
+        Species = new SummoningValue(species);
+        Elements = new SummoningValue(elements);
+    }
+}
