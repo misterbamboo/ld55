7ca5b7b1210693c028c6e5fab0a5771e860e7f57
Mister Bamboo <nicolascnaud1989@gmail.com>
2024-04-15 11:49:48 -0400

    UI: Change background and candles

--- src/GameManager/candles/CandlesManager.cs
+++ src/GameManager/candles/CandlesManager.cs
@@ -1,42 +1,66 @@
 using Godot;
-using System;
 using System.Linq;
 
 public partial class CandlesManager : Node
 {
-	private DeskManager deskManager;
+    private DeskManager deskManager;
 
-	private bool[] candles = new bool[5];
-	[Export] private Godot.Collections.Array<TextureRect> candleTextures;
-	[Export] private Texture2D litCandle;
-	[Export] private Texture2D unlitCandle;
+    private bool[] candles = new bool[5];
+    private bool[] candlesHover = new bool[5];
+    [Export] private Godot.Collections.Array<TextureRect> candleTextures;
+    [Export] private Texture2D litCandle;
+    [Export] private Texture2D litCandleHover;
+    [Export] private Texture2D unlitCandle;
+    [Export] private Texture2D unlitCandleHover;
 
-
-    // Called when the node enters the scene tree for the first time.
     public override void _Ready()
-	{
-		deskManager = GetNode<DeskManager>(DeskManager.Path);
-		DrawCandles();
-	}
+    {
+        deskManager = GetNode<DeskManager>(DeskManager.Path);
+        DrawCandles();
+    }
 
-	public void ToggleCandle(int index)
-	{
-		candles[index] = !candles[index];
-		DrawCandles();
+    public void ChangeHoverCandle(int index, bool hover)
+    {
+        candlesHover[index] = hover;
+        DrawCandles();
+    }
 
-		if(candles.All(x => x))
-		{
-			deskManager.SummonMonster();
-			candles = new bool[5];
+    public void ToggleCandle(int index)
+    {
+        candles[index] = !candles[index];
+        DrawCandles();
+
+        if (candles.All(x => x))
+        {
+            deskManager.SummonMonster();
+            candles = new bool[5];
             DrawCandles();
         }
-	}
+    }
 
-	public void DrawCandles()
-	{
-		for (var i = 0; i < candleTextures.Count;  i++)
-		{
-			candleTextures[i].Texture = candles[i] ? litCandle : unlitCandle;
-		}
-	}
+    public void DrawCandles()
+    {
+        for (var i = 0; i < candleTextures.Count; i++)
+        {
+            candleTextures[i].Texture = candles[i] ? GetLitCandle(i) : GetUnlitCandle(i);
+        }
+    }
+
+    private Texture2D GetLitCandle(int index)
+    {
+        if (candlesHover[index])
+        {
+            return litCandleHover;
+        }
+        return litCandle;
+    }
+
+    private Texture2D GetUnlitCandle(int index)
+    {
+        if (candlesHover[index])
+        {
+            return unlitCandleHover;
+        }
+        return unlitCandle;
+    }
 }
