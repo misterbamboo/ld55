06da7d9d700dcab296e451f15572e2537e384066
Philippe Côté <phil@bleuet.design>
2024-04-15 18:07:50 -0400

    Made everything bigger, added lot of UI-UX polish.

--- src/Contexts/Combat/CombatZone/CombatSummonZoneUI.cs
+++ src/Contexts/Combat/CombatZone/CombatSummonZoneUI.cs
@@ -1,68 +1,68 @@
-﻿
+
 using Godot;
 using System;
 
 public partial class CombatSummonZoneUI : Control
 {
-    public event Action<MonsterCardUI> OnReadyToFight;
+	public event Action<MonsterCardUI> OnReadyToFight;
 
-    private ControlDragHandler DragHandler { get; set; }
-    public CombatSummonZoneUI()
-    {
-        DragHandler = new ControlDragHandler(this, allowDrag: false);
-    }
+	private ControlDragHandler DragHandler { get; set; }
+	public CombatSummonZoneUI()
+	{
+		DragHandler = new ControlDragHandler(this, allowDrag: false);
+	}
 
-    private MonsterCardUI playerStickCard;
+	private MonsterCardUI playerStickCard;
 
-    public override void _Ready()
-    {
-        DragHandler.OnStartMouseHover += DragHandler_OnStartMouseHover;
-        DragHandler.OnEndMouseHover += DragHandler_OnEndMouseHover;
-    }
+	public override void _Ready()
+	{
+		DragHandler.OnStartMouseHover += DragHandler_OnStartMouseHover;
+		DragHandler.OnEndMouseHover += DragHandler_OnEndMouseHover;
+	}
 
-    private void DragHandler_OnStartMouseHover()
-    {
-        var card = PlayerHand.Instance.FirstCardInHand();
-        if (card == null)
-        {
-            return;
-        }
+	private void DragHandler_OnStartMouseHover()
+	{
+		var card = PlayerHand.Instance.FirstCardInHand();
+		if (card == null)
+		{
+			return;
+		}
 
-        playerStickCard = card;
-        playerStickCard.TriggerStickToAnim(GlobalPosition + new Vector2(2, 2), StickAnimEnded);
-    }
+		playerStickCard = card;
+		playerStickCard.TriggerStickToAnim(GlobalPosition + new Vector2(2, 2), StickAnimEnded);
+	}
 
-    private void StickAnimEnded()
-    {
-        if (playerStickCard == null)
-        {
-            return;
-        }
+	private void StickAnimEnded()
+	{
+		if (playerStickCard == null)
+		{
+			return;
+		}
 
-        if (!PlayerHand.Instance.InHand(playerStickCard))
-        {
-            OnReadyToFight?.Invoke(playerStickCard);
-        }
+		if (!PlayerHand.Instance.InHand(playerStickCard))
+		{
+			OnReadyToFight?.Invoke(playerStickCard);
+		}
 
-        playerStickCard = null;
-    }
+		playerStickCard = null;
+	}
 
-    private void DragHandler_OnEndMouseHover()
-    {
-        if (playerStickCard == null)
-        {
-            return;
-        }
+	private void DragHandler_OnEndMouseHover()
+	{
+		if (playerStickCard == null)
+		{
+			return;
+		}
 
-        playerStickCard.ReleaseStickToAnim();
-    }
+		playerStickCard.ReleaseStickToAnim();
+	}
 
-    public override void _Process(double delta)
-    {
-    }
+	public override void _Process(double delta)
+	{
+	}
 
-    public override void _Input(InputEvent @event)
-    {
-        DragHandler.HandleInput(@event);
-    }
+	public override void _Input(InputEvent @event)
+	{
+		DragHandler.HandleInput(@event);
+	}
 }
